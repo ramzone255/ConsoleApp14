a8cb294a295b1da38d97b0b64d75d7567eb8b03d
Tryals <killerzo@yandex.com>
2023-03-29 09:50:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"Lines.txt", Encoding.UTF8);
+            Console.WriteLine(sr.ReadToEnd());
+            string str = File.ReadLines("Lines.txt").First();
+            str = string.Concat(str.Select(c => (char)(char.ToLower(c) ^ char.ToUpper(c) ^ c)));
+            StreamWriter sw = new StreamWriter(@"Rezult.txt");
+            sw.WriteLine(str);
+            sw.Close();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
